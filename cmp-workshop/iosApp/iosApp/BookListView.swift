3e3bc7138bcd3d2cb6a3c850aa49353b4cfe1d92
Dominik Chmelík <chmelik.dominik@icloud.com>
2025-11-04 17:04:37 +0100

    Prepare cmp initial

---
+++ cmp-workshop/iosApp/iosApp/BookListView.swift
@@ -0,0 +1,26 @@
+//
+// Created by Dominik Chmelík on 14.10.2025.
+//
+
+import SwiftUI
+
+struct BookListView: View {
+    @State private var viewModel: BookViewModel = BookViewModel()
+    @State private var presentedBook: BookItem?
+
+    var body: some View {
+        List(viewModel.books) { book in
+            Text(book.name)
+                .fontWeight(.heavy)
+                .onTapGesture {
+                    presentedBook = book
+                }
+        }
+        .task {
+            viewModel.fetchBooks()
+        }
+        .sheet(item: $presentedBook) { book in
+            BookDetailView(viewModel: $viewModel, book: book)
+        }
+    }
+}
